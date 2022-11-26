704a84624d033d54425e533b75c70e8272099e3f
kylemondina <kylemondinaportfolio@gmail.com>
2022-11-26 15:27:30 -0800

    fixing for server deployment

--- phase5/main.cpp
+++ phase5/main.cpp
@@ -14,14 +14,14 @@
 int main(){
 	
 	/*
-	NodeList nodes("./Users.txt");
+	NodeList nodes("./Users1.txt");
 	nodes.show();
 	Server s(1,8000,100,1000);
 	s.start(nodes);
 	
 	*/
 
-	Client c1(1, "54.149.66.46", 8000);
+	Client c1(1, "34.208.184.118", 8000);
 	Message msg(3,4,"post","I am at SCU");
 	c1.send_msg(msg);
 	//c2.send_msg("hi 2 from local test");
