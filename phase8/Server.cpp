7066fb037e494fb33dbcf0b67c1101a18180320b
kylemondina <kylemondinaportfolio@gmail.com>
2022-12-01 08:19:50 -0800

    fixed stoi breaking by padding checking end of the message delimter and more effective suppression of the ping message

--- phase8/Server.cpp
+++ phase8/Server.cpp
@@ -33,44 +33,6 @@ Server::Server(int g, int p, int sq, int bs):
 	std::cout << "Server Now Listening To TCP Socket: " << sockfd << std::endl;	
 }
 
-void Server::handle_client_node(int connfd, int server_buffer_size, NodeList *nodes){
-	int client_socket = connfd;
-	int valread;
-	char server_buffer[server_buffer_size] = {0};
-
-	while(1){
-		//clear the buffer
-		memset(server_buffer,' ',server_buffer_size);
-
-		//read the message from client socket
-		if((valread = read(client_socket,server_buffer,server_buffer_size))<0){
-			std::cout << "server failed to read from client: " << client_socket << std::endl;
-		}
-		if(valread == 0){
-			break;
-		}
-
-		//parse the message
-		std::string recieved_message(server_buffer);
-		Message parsed_msg = parse_msg(recieved_message);
-
-		if (parsed_msg.mtype() == "post"){
-			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
-			std::cout << "Sending Post to All Nodes" << std::endl;
-			nodes->send_to_all(recieved_message);
-		}else if (parsed_msg.mtype() == "dm"){
-			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
-			std::cout << "sguid: " << parsed_msg.msguid()<< " sent a message to: " << 
-			parsed_msg.mtguid()<< " saying: " << parsed_msg.mmsg() << std::endl;
-			nodes->send_to(parsed_msg.mtguid(),parsed_msg);
-		}else if (parsed_msg.mtype() == "ping"){
-			//No Action Necessary, Message Sent was a Failure Detection
-			//return;
-		}
-		std::cout << "Recieved Message: " << recieved_message << std::endl;
-	}
-}
-
 void Server::handle_client(int connfd, int server_buffer_size){
 	int client_socket = connfd;
 	int valread;
@@ -89,13 +51,55 @@ void Server::handle_client(int connfd, int server_buffer_size){
 		}
 
 		//parse the message
-		std::string recieved_message(server_buffer);
+		std::string raw_recieved_message(server_buffer);
+		std::string recieved_message = raw_recieved_message.substr(0,raw_recieved_message.find("~")+1);
 		Message parsed_msg = parse_msg(recieved_message);
-		if (parsed_msg.mtype() == "ping"){
-			//No Action Necessary, Message Sent was a Failure Detection
+
+		if (parsed_msg.mtype() != "ping"){
+			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
+			std::cout << "Recieved Message: " << recieved_message << std::endl;
 		}
-		std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
-		std::cout << "Recieved Message: " << recieved_message << std::endl;
+
+	}
+}
+
+void Server::handle_client_node(int connfd, int server_buffer_size, NodeList *nodes){
+	int client_socket = connfd;
+	int valread;
+	char server_buffer[server_buffer_size] = {0};
+
+	while(1){
+		//clear the buffer
+		memset(server_buffer,' ',server_buffer_size);
+
+		//read the message from client socket
+		if((valread = read(client_socket,server_buffer,server_buffer_size))<0){
+			std::cout << "server failed to read from client: " << client_socket << std::endl;
+		}
+		if(valread == 0){
+			break;
+		}
+
+		//parse the message
+		std::string raw_recieved_message(server_buffer);
+		std::string recieved_message = raw_recieved_message.substr(0,raw_recieved_message.find("~")+1);
+		Message parsed_msg = parse_msg(recieved_message);
+
+		if (parsed_msg.mtype() != "ping"){
+			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
+			std::cout << "Recieved Message: " << recieved_message << std::endl;
+		}
+
+		if (parsed_msg.mtype() == "post"){
+			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
+			std::cout << "Sending Post to All Nodes" << std::endl;
+			nodes->send_to_all(recieved_message);
+		}else if (parsed_msg.mtype() == "dm"){
+			std::cout << "Message Type: " << parsed_msg.mtype() << std::endl;
+			std::cout << "sguid: " << parsed_msg.msguid()<< " sent a message to: " << 
+			parsed_msg.mtguid()<< " saying: " << parsed_msg.mmsg() << std::endl;
+			nodes->send_to(parsed_msg.mtguid(),parsed_msg);
+		}	
 	}
 }
 
@@ -143,18 +147,13 @@ void Server::start_server_online(bool *server_online)
 	struct sockaddr_in clientAddr;
 	socklen_t alen;
 	while (1){
-		std::cout << "Server Ready to Accept Again" << std::endl;
-		std::cout << "Code Before the Accept" << std::endl;
 		if ((connfd = accept(sockfd, (struct sockaddr *)&clientAddr, &alen))<0){
 			std::cout << "Accept Failed" << std::endl;
 			perror("Node Server Unable to Accept Client\n");
 			exit(1);
 		}
-		std::cout << "Code After Accept" << std::endl;
-		std::cout << "Node Server Accepted Client " << connfd << std::endl;
 		if (*server_online == true){
 			std::cout << "Central Server is Online, using COSN Handler" << std::endl;
-			//handle_client(connfd,server_buffer_size);
 			std::thread t(handle_client,connfd,server_buffer_size);
 			t.detach();
 		}else{
