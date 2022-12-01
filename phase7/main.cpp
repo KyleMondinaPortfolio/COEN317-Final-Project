8fc1cb1aa81cb61fe81d832917e3d128dc717026
kylemondina <kylemondinaportfolio@gmail.com>
2022-11-30 21:25:33 -0800

    added different dedicated ports

--- phase7/main.cpp
+++ phase7/main.cpp
@@ -34,44 +34,55 @@ int main(){
 
 	if (server_type == 0){
 		Client c1(1, "54.149.66.46", 8000);
-		Message msg("post",0,3,"count dankula");
+		//Client c1(1, "34.208.184.118", 8000);
+		//Client c1(1, "54.202.116.197", 8000);
+		Message msg("post",0,3,"what is going on");
 		c1.send_msg(msg);
 		return 0;
 	}
 	else if (server_type == 1){
 		//Node Client Code
 		bool server_online = false;
-		Client central_server(1, "54.149.66.46", 8000);
-		Server self(guid,8000,100,1000);
+		Client central_server(1, "54.149.66.46", 9000);
+		Server self_main(guid,8000,100,1000);
+		Server self_heart(guid,9000,100,1000);
 
 		//Failure Detector for Central Server
 		std::mutex monitor_mtx;
 		std::thread t1(&Client::monitor_failure,&central_server,true,3,&monitor_mtx,&server_online);
 
-		//User Interface
-		std::thread t2(user_interface, &central_server, &server_online);
-
 		//Background Listening Process
-		std::thread t3(&Server::start_server_online, &self, &server_online);
+		//For Failure Detection
+		std::thread t2(&Server::start_server_online, &self_heart, &server_online);
+		//Main Listener
+		std::thread t3(&Server::start, &self_main);
 
 		t1.join();
 		t2.join();
 		t3.join();
 		return 0;
+
 	}else if (server_type == 2){
 		//Central Server Code
 		NodeList nodes("./Users.txt");
+		NodeList heart_beats("./Detect.txt");
 		nodes.show();
-		Server s(1,8000,100,1000);
+		heart_beats.show();
+	
+		Server self_main(1,8000,100,1000);
+		Server self_heart(1,9000,100,1000);
 
-		//Background Listening Process
-		std::thread t1(&Server::start_node,&s,&nodes);
+		//for port 8000
+		std::thread t1(&Server::start_node,&self_main,&nodes);
+		//for port 9000
+		std::thread t2(&Server::start,&self_heart);
 
 		//Nodes Failure Detector
-		std::thread t2(&NodeList::monitor_failures,&nodes,false,3);
-	
+		std::thread t3(&NodeList::monitor_failures,&heart_beats,false,3);
+		
 		t1.join();
 		t2.join();
+		t3.join();
 		return 0;
 	}else{
 		std::cout << "Server Type Not Specified" <<std::endl;
